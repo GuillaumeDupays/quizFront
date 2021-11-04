2f379ccdc88ca81802b1d74079d5a727d5877d10
Guillaume Dupays <guillaumedupays@gmail.com>
2021-11-04 11:19:33 +0100

    refactoring code

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -10,81 +10,56 @@ import { Directive, Input, Output, EventEmitter } from '@angular/core';
 export class QuestionsComponent implements OnInit {
   selectedMessage: any;
   datasByPage: any = {};
-  currentQuestion: any = {};
+  // change question and response dynamically
   questionCounter: any = 0;
+  // question by theme
+  currentQuestion: any = {};
+  // answer by theme and question
   currentAnswer: any = [];
-  answerCounter: any = 0;
+  // determine a value for each response
+  resA: string = '';
+  resB: string = '';
+  resC: string = '';
   @Input() response: string = '';
-  resA: string[] = [];
-  resB: string[] = [];
-  resC: string[] = [];
-  constructor(private quizDataService: QuizInformations) {}
-  // responseA: string = this.currentQuestion.responses.a;
+
+  constructor() {}
   ngOnInit() {
-    this.quizDataService.currentMessage.subscribe(
-      (msg) => (this.selectedMessage = msg + 'banane')
-    );
     this.getDatasQuizByPage();
     this.currentAnswer = this.datasByPage.questionsById[this.questionCounter];
-    console.log('this.currentAnswer :>> ', this.currentAnswer);
-    // this.goodAnswersCounter();
-    const currResA = Object.keys(this.currentAnswer.responses[0]);
-    console.log('currResA :>> ', currResA);
-    const resA = currResA.filter((e: any) => e === 'a');
-    console.log('resA :>> ', resA);
-    const resB = currResA.filter((e: any) => e === 'b');
-    this.resA = resA;
-    console.log('resB :>> ', resB);
-    this.resB = resB;
-    const resC = currResA.filter((e: any) => e === 'c');
-    console.log('resC :>> ', resC);
-    this.resC = resC;
-    console.log('this.responseA :>> ', this.resA);
+    this.initResponses();
   }
-  getQuiz() {
-    this.quizDataService.changeMessage('tape');
-  }
-
   ngAfterViewInit() {}
-  selectResponse(response: string[]) {
+  initResponses() {
+    const currAnswer = Object.keys(this.currentAnswer.responses[0]);
+    currAnswer.filter((response: string) => {
+      response === 'a' ? (this.resA = response) : -1,
+        response === 'b' ? (this.resB = response) : -1,
+        response === 'c' ? (this.resC = response) : -1;
+    });
+  }
+  findTheGoodResponse(response: string) {
     const goodRes = this.currentAnswer.responses[0].goodRes;
-    console.log('goodRes :>> ', goodRes);
-    if (response.toString() === goodRes) {
-      console.log('GOOD :>> ');
+    response === goodRes
+      ? response === this.resA
+      : response === this.resB || this.resC;
+    if (response === goodRes) {
+      console.log('good :>> ', response);
     } else {
-      console.log('BAD :>> ');
+      console.log('Bad :>> ', response);
     }
-    if (response === this.resA) {
-      console.log('this.resA :>> ', this.resA);
-    }
-    if (response === this.resB) {
-      console.log('this.resB :>> ', this.resB);
-    }
-    if (response === this.resC) {
-      console.log('this.resC :>> ', this.resC);
-    }
+  }
+  selectResponse(response: string) {
+    this.findTheGoodResponse(response);
   }
   nextQuestion() {
-    this.goodAnswersCounter();
     this.questionCounter += 1;
     this.currentQuestion = this.datasByPage.questionsById[this.questionCounter];
     this.currentAnswer = this.datasByPage.questionsById[this.questionCounter];
   }
-
-  goodAnswersCounter() {
-    const qById =
-      this.datasByPage.questionsById[this.questionCounter].idQuestion;
-    console.log('qById :>> ', qById);
-    // if (goodReponse) {
-    //   console.log('good :>> ', good);
-    // } else {
-    //   console.log('bad :>> ', bad);
-    // }
-  }
-
   getDatasQuizByPage() {
+    // get from quiz component all data you need to questions, answers...
+    // second affect to current var, datas correponding like currentQuestion, currentAnswer etc...
     this.datasByPage = history.state;
-    console.log('this.datasByPage :>> ', this.datasByPage);
     this.currentQuestion = this.datasByPage.questionsById[this.questionCounter];
   }
 }
