23be14735ac37ab066b80a776b58e0382a0c95d6
Guillaume Dupays <guillaumedupays@gmail.com>
2021-11-09 11:43:09 +0100

    form improvement

--- src/app/manage-questions/manage-questions.component.ts
+++ src/app/manage-questions/manage-questions.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Question } from '../models/question';
@@ -10,14 +11,15 @@ import { QuizInformations } from '../services/quizInformations';
 })
 export class ManageQuestionsComponent implements OnInit {
   form: FormGroup;
-  numberQuestion: number = 1;
+  numberQuestion: number = 0;
+  isThemeChosen: boolean = false;
+  quizTheme: string = '';
   constructor(
     private fb: FormBuilder,
     private questionService: QuizInformations
   ) {
     this.form = this.fb.group({
       theme: ['', Validators.required],
-      numberQuestion: ['', Validators.required],
       contentQuestion: ['', Validators.required],
       responseA: ['', Validators.required],
       responseB: ['', Validators.required],
@@ -26,17 +28,22 @@ export class ManageQuestionsComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.numberQuestion = 1;
+  }
+
   addQuestion() {
     const idQuiz = 1;
     const newIdQuestion = this.numberQuestion
       .toString()
       .concat(idQuiz.toString());
     const form = this.form.value;
-    console.log('form :>> ', form);
+    form.theme ? (this.quizTheme = form.theme) : (form.theme = this.quizTheme);
+
     const newQuestion: Question = {
+      autor: 'Guillaume',
+      theme: this.quizTheme,
       idQuiz: idQuiz,
-      theme: form.theme,
       idQuestion: parseInt(newIdQuestion),
       numQuestion: this.numberQuestion,
       contentQuestion: form.contentQuestion,
@@ -52,6 +59,7 @@ export class ManageQuestionsComponent implements OnInit {
     console.log('newQuestion :>> ', newQuestion);
     this.form.reset();
     // this.questionService.createQuestion(newQuestion).subscribe();
+    this.isThemeChosen = true;
     this.numberQuestion += 1;
   }
 }
--- src/app/models/question.ts
+++ src/app/models/question.ts
@@ -1,4 +1,5 @@
 export interface Question {
+  autor?: String;
   theme?: String;
   idQuiz: Number;
   idQuestion: Number;
